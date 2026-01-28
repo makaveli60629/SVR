d0ddf9c6416907e508c1b5b37542a13ecc90a852
makaveli60629 <ronaldchadee@gmail.com>
2026-01-27 21:40:29 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 // /index.js — ROOT ENTRY (PERMANENT)
-// Loads Scarlett1 world always.
-
 import { Scarlett1 } from "./js/scarlett1/index.js";
-
-console.log("🚀 ROOT index.js loaded");
 Scarlett1.start();
