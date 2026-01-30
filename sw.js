81c5b91c95cb5bcceec606b8b796d7cef2d71f84
makaveli60629 <ronaldchadee@gmail.com>
2026-01-29 21:28:01 -0600

    Delete sw.js⁷

--- sw.js
+++
@@ -1,3 +0,0 @@
-// sw.js — minimal, safe (no caching to avoid stale builds)
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
