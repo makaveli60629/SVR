348d154a17d69f5ebf7047857652a2d8b1e93900
makaveli60629 <ronaldchadee@gmail.com>
2026-01-31 21:01:38 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// SVR root entry (NO bare imports)
+// Loads runtime spine which loads the Scarlett1 world.
+// Permanent folder layout: /SVR/js/runtime + /SVR/js/scarlett1
 import { Spine } from "./js/runtime/spine.js";
 
-console.log("🚀 SCARLETT: starting spine...");
 Spine.start();
