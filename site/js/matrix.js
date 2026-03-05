02d274e621f8325f3a58e9e55e5e410632adcd09
makaveli60629 <ronaldchadee@gmail.com>
2026-03-05 17:58:32 -0600

    Add files via upload

--- site/js/matrix.js
+++ site/js/matrix.js
@@ -1,32 +1,44 @@
-const canvas = document.getElementById("matrix")
-const ctx = canvas.getContext("2d")
 
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
+const canvas=document.createElement("canvas")
+document.body.appendChild(canvas)
 
-const letters = "01SVRPOKER"
-const fontSize = 14
-const columns = canvas.width/fontSize
+const ctx=canvas.getContext("2d")
 
-const drops = []
+canvas.style.position="fixed"
+canvas.style.top=0
+canvas.style.left=0
+canvas.style.zIndex=-1
 
-for(let x=0;x<columns;x++)
-    drops[x]=1
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+
+const letters="SVR"
+const fontSize=16
+const columns=canvas.width/fontSize
+const drops=[]
+
+for(let x=0;x<columns;x++) drops[x]=1
 
 function draw(){
-    ctx.fillStyle="rgba(0,0,0,0.05)"
-    ctx.fillRect(0,0,canvas.width,canvas.height)
-    
-    ctx.fillStyle="#a020f0"
-    ctx.font=fontSize+"px monospace"
-    
-    for(let i=0;i<drops.length;i++){
-        const text=letters.charAt(Math.floor(Math.random()*letters.length))
-        ctx.fillText(text,i*fontSize,drops[i]*fontSize)
-        if(drops[i]*fontSize>canvas.height && Math.random()>0.975)
-            drops[i]=0
-        drops[i]++
-    }
+
+ctx.fillStyle="rgba(0,0,0,0.05)"
+ctx.fillRect(0,0,canvas.width,canvas.height)
+
+ctx.fillStyle="#a020f0"
+ctx.font=fontSize+"px monospace"
+
+for(let i=0;i<drops.length;i++){
+
+const text=letters[Math.floor(Math.random()*letters.length)]
+ctx.fillText(text,i*fontSize,drops[i]*fontSize)
+
+if(drops[i]*fontSize>canvas.height && Math.random()>0.975)
+drops[i]=0
+
+drops[i]++
+
+}
+
 }
 
 setInterval(draw,35)
