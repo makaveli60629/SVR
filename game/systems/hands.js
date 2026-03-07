43f53672eb99cd608b98ca6c07f900c1aa48b29c
makaveli60629 <ronaldchadee@gmail.com>
2026-03-07 04:34:53 -0600

    Update hands.js

--- game/systems/hands.js
+++ game/systems/hands.js
@@ -1,13 +1,5 @@
-AFRAME.registerComponent('vr-hands', {
+export function initHands(scene){
 
-    init: function () {
-        const scene = this.el
-        const left = document.createElement('a-entity')
-        left.setAttribute('hand-controls', 'left')
-        scene.appendChild(left)
-        const right = document.createElement('a-entity')
-        right.setAttribute('hand-controls', 'right')
-        scene.appendChild(right)
-    }
+console.log("VR hands system ready");
 
-})
+}
