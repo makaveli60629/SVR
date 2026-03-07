cf9d4eb2a6ac6abb29d18dd23b156bb93853ddf3
makaveli60629 <ronaldchadee@gmail.com>
2026-03-07 04:33:54 -0600

    Update seat-system.js

--- game/scripts/seat-system.js
+++ game/scripts/seat-system.js
@@ -1,6 +1,32 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.161/build/three.module.js";
 
-AFRAME.registerComponent('seat-system',{
-init:function(){
-console.log("Seat system initialized");
-}
+export function initSeats(scene){
+
+const seats = [
+
+[0,0,7],
+[6,0,4],
+[6,0,-4],
+[0,0,-7],
+[-6,0,-4],
+[-6,0,4]
+
+];
+
+seats.forEach(pos=>{
+
+const seat = new THREE.Mesh(
+
+new THREE.BoxGeometry(1.2,0.6,1.2),
+
+new THREE.MeshStandardMaterial({color:0x333333})
+
+);
+
+seat.position.set(pos[0],0.3,pos[2]);
+
+scene.add(seat);
+
 });
+
+}
