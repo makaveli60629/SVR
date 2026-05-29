272d0180d9631ec79faac625869d80fdb2135d8a
makaveli60629 <ronaldchadee@gmail.com>
2026-05-28 20:34:50 -0500

    Phase 97 ensure north sky Moon Mars in lobby

--- game/modules/phase93_bootstrap.js
+++ game/modules/phase93_bootstrap.js
@@ -1,37 +1,38 @@
 import { installPhase93LobbyRepair } from "./phase93_lobby_repair.js";
-import { textureExistingMoonMars, tickTexturedMoonMars } from "./moon_mars_textured.js";
+import { textureExistingMoonMars, ensureNorthSkyMoonMars, tickTexturedMoonMars } from "./moon_mars_textured.js";
 
-const PHASE95 = "PHASE-95-TEXTURED-MOON-MARS-ALL-ROOMS-LOCK";
+const PHASE97 = "PHASE-97-NORTH-SKY-MOON-MARS-VISIBLE-LOCK";
 
 async function loadThree(){
   try{
     return await import("three");
   }catch(err){
-    console.warn(`[${PHASE95}] three import failed`, err);
+    console.warn(`[${PHASE97}] three import failed`, err);
     return null;
   }
 }
 
 const THREE = await loadThree();
 
-if (THREE?.WebGLRenderer && !THREE.WebGLRenderer.prototype.__phase95Patched){
+if (THREE?.WebGLRenderer && !THREE.WebGLRenderer.prototype.__phase97Patched){
   const originalRender = THREE.WebGLRenderer.prototype.render;
-  THREE.WebGLRenderer.prototype.__phase95Patched = true;
+  THREE.WebGLRenderer.prototype.__phase97Patched = true;
   THREE.WebGLRenderer.prototype.render = function(scene, camera){
     try{
       if (scene && !scene.userData?.phase93RepairApi){
         installPhase93LobbyRepair({ scene, world: null, log: console.log, selfTick: true });
       }
-      if (scene && !scene.userData?.phase95TexturedPlanets){
+      if (scene && !scene.userData?.phase97NorthSkyPlanets){
         textureExistingMoonMars(scene);
-        scene.userData.phase95TexturedPlanets = true;
+        ensureNorthSkyMoonMars(scene);
+        scene.userData.phase97NorthSkyPlanets = true;
       }
       if (scene) tickTexturedMoonMars(scene, 0.016);
       if (scene?.userData) scene.userData._camera = camera;
     }catch(err){
-      console.warn(`[${PHASE95}] render hook repair failed`, err);
+      console.warn(`[${PHASE97}] render hook repair failed`, err);
     }
     return originalRender.call(this, scene, camera);
   };
-  console.log(`[${PHASE95}] render hook installed`);
+  console.log(`[${PHASE97}] render hook installed`);
 }
