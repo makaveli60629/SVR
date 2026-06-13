861700b66f3f0ecc583a0f59e85bd4c57fa332a4
makaveli60629 <ronaldchadee@gmail.com>
2026-06-13 11:44:36 -0500

    Phase 149 wire visual refinement

--- game/modules/reiki_phase119_trueitive_storefront_final.js
+++ game/modules/reiki_phase119_trueitive_storefront_final.js
@@ -1,5 +1,8 @@
 import { applyPhase143WallAlignedSkylineAds } from "./phase143_wall_aligned_skyline_ads.js";
+import { applyPhase149VisualRefinement } from "./phase149_visual_refinement.js";
 
-export function applyPhase119ReikiTrueitiveStorefrontFinal(args = {}){
-  return applyPhase143WallAlignedSkylineAds(args);
+export async function applyPhase119ReikiTrueitiveStorefrontFinal(args = {}){
+  const result = await applyPhase143WallAlignedSkylineAds(args);
+  applyPhase149VisualRefinement(args, result);
+  return result;
 }
