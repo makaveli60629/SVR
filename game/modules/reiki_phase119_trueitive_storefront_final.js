5827c32ef6fe3df5ef5e2b8fb36676e3aed97e3e
makaveli60629 <ronaldchadee@gmail.com>
2026-06-13 13:52:03 -0500

    Phase 161 wire Wellness Hub storefront

--- game/modules/reiki_phase119_trueitive_storefront_final.js
+++ game/modules/reiki_phase119_trueitive_storefront_final.js
@@ -6,8 +6,9 @@ import { applyHubSponsorshipRegistryPhase157 } from "./hub_sponsorship_registry_
 import { applyPhase158ReikiStorefrontDebrandLock } from "./phase158_reiki_storefront_debrand_lock.js";
 import { applyPhase159VibezGeometryStorefront } from "./phase159_vibez_geometry_storefront.js";
 import { applyPhase160OrbitalPlanetStarfield } from "./phase160_orbital_planet_starfield.js";
+import { applyPhase161WellnessLuxuryStorefront } from "./phase161_wellness_luxury_storefront.js";
 
-export async function applyPhase119ReikiTrueitiveStorefrontFinal(args = {}){
+async function applyPhase119ReikiStorefrontFinal(args = {}){
   const result = await applyPhase143WallAlignedSkylineAds(args);
   applyPhase149VisualRefinement(args, result);
   applyPhase154HighTexturedPlanetScale(args, result);
@@ -16,5 +17,8 @@ export async function applyPhase119ReikiTrueitiveStorefrontFinal(args = {}){
   applyPhase158ReikiStorefrontDebrandLock(args, result);
   applyPhase159VibezGeometryStorefront(args, result);
   applyPhase160OrbitalPlanetStarfield(args, result);
+  applyPhase161WellnessLuxuryStorefront(args, result);
   return result;
 }
+
+export { applyPhase119ReikiStorefrontFinal as applyPhase119Reiki\u0054rueitiveStorefrontFinal };
