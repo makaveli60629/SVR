0bc807bbffee31b5795d876a0df1cd8d073fba74
makaveli60629 <ronaldchadee@gmail.com>
2026-06-13 13:04:16 -0500

    Phase 157 wire modular hub sponsorship registry

--- game/modules/reiki_phase119_trueitive_storefront_final.js
+++ game/modules/reiki_phase119_trueitive_storefront_final.js
@@ -2,11 +2,13 @@ import { applyPhase143WallAlignedSkylineAds } from "./phase143_wall_aligned_skyl
 import { applyPhase149VisualRefinement } from "./phase149_visual_refinement.js";
 import { applyPhase154HighTexturedPlanetScale } from "./phase154_high_textured_planet_scale.js";
 import { applyPhase155SkylineAdRingMoonGlow } from "./phase155_skyline_ad_ring_moon_glow.js";
+import { applyHubSponsorshipRegistryPhase157 } from "./hub_sponsorship_registry_phase157.js";
 
 export async function applyPhase119ReikiTrueitiveStorefrontFinal(args = {}){
   const result = await applyPhase143WallAlignedSkylineAds(args);
   applyPhase149VisualRefinement(args, result);
   applyPhase154HighTexturedPlanetScale(args, result);
   applyPhase155SkylineAdRingMoonGlow(args, result);
+  applyHubSponsorshipRegistryPhase157(args, result);
   return result;
 }
