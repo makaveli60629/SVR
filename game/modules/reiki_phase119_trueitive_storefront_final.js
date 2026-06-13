97a17175ac511099cdf3e16735038b52c32123b2
makaveli60629 <ronaldchadee@gmail.com>
2026-06-13 12:30:35 -0500

    Phase 154 wire high textured planet scale correction

--- game/modules/reiki_phase119_trueitive_storefront_final.js
+++ game/modules/reiki_phase119_trueitive_storefront_final.js
@@ -1,8 +1,10 @@
 import { applyPhase143WallAlignedSkylineAds } from "./phase143_wall_aligned_skyline_ads.js";
 import { applyPhase149VisualRefinement } from "./phase149_visual_refinement.js";
+import { applyPhase154HighTexturedPlanetScale } from "./phase154_high_textured_planet_scale.js";
 
 export async function applyPhase119ReikiTrueitiveStorefrontFinal(args = {}){
   const result = await applyPhase143WallAlignedSkylineAds(args);
   applyPhase149VisualRefinement(args, result);
+  applyPhase154HighTexturedPlanetScale(args, result);
   return result;
 }
