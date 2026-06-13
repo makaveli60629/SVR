d4f952a9bdf8e40742e2aef74a6838c4688d01da
makaveli60629 <ronaldchadee@gmail.com>
2026-06-13 12:35:12 -0500

    Phase 155 wire skyline ad ring moon glow

--- game/modules/reiki_phase119_trueitive_storefront_final.js
+++ game/modules/reiki_phase119_trueitive_storefront_final.js
@@ -1,10 +1,12 @@
 import { applyPhase143WallAlignedSkylineAds } from "./phase143_wall_aligned_skyline_ads.js";
 import { applyPhase149VisualRefinement } from "./phase149_visual_refinement.js";
 import { applyPhase154HighTexturedPlanetScale } from "./phase154_high_textured_planet_scale.js";
+import { applyPhase155SkylineAdRingMoonGlow } from "./phase155_skyline_ad_ring_moon_glow.js";
 
 export async function applyPhase119ReikiTrueitiveStorefrontFinal(args = {}){
   const result = await applyPhase143WallAlignedSkylineAds(args);
   applyPhase149VisualRefinement(args, result);
   applyPhase154HighTexturedPlanetScale(args, result);
+  applyPhase155SkylineAdRingMoonGlow(args, result);
   return result;
 }
