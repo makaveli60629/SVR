a7cd8de2bd10168a92243ecb9b31dfa0fb421b19
makaveli60629 <ronaldchadee@gmail.com>
2026-06-14 18:37:37 -0500

    Phase 232 chain stable lite polish

--- game/phase229_final_phase_label_moon_realign_lock.js
+++ game/phase229_final_phase_label_moon_realign_lock.js
@@ -1,1 +1,2 @@
-import "./phase230_moon_off_table_phase_label_lock.js?v=phase232-stable-moon-only";
+import "./phase230_moon_off_table_phase_label_lock.js?v=phase232-stable-moon";
+import "./phase232_stable_lite_polish_lock.js?v=phase232-stable-lite-polish";
