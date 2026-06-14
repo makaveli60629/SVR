8aefd221cd243fc648923f7b37fbbcb8902cad8a
makaveli60629 <ronaldchadee@gmail.com>
2026-06-13 19:42:07 -0500

    Wire Phase 188 visible second floor

--- game/phase176_boot.js
+++ game/phase176_boot.js
@@ -3,13 +3,14 @@ import { installPhase178Bounds } from "./modules/phase178_bounds.js";
 import { autoInstallPhase180TableSelector } from "./modules/phase180_table_selector.js";
 import { autoInstallPhase185OfficialLobbyLook } from "./modules/phase185_official_lobby_look.js";
 import { installPhase186DeploySyncCleanup } from "./modules/phase186_deploy_sync_cleanup.js";
+import { autoInstallPhase188SecondFloorVisibility } from "./modules/phase188_second_floor_visibility.js";
 
-const LABEL = "UPDATE-3.0-PHASE-187-OFFICIAL-LOBBY-STABILIZER-LOCK";
+const LABEL = "UPDATE-3.0-PHASE-188-SECOND-FLOOR-VISIBILITY-LOCK";
 
 function sync(){
   window.SVR_PHASE106 = window.SVR_PHASE106 || {};
   window.SVR_PHASE106.build = LABEL;
-  window.SVR_PHASE187 = Object.assign(window.SVR_PHASE187 || {}, { build: LABEL, active: true, boot: true });
+  window.SVR_PHASE188 = { build: LABEL, active: true, secondFloor: true };
   document.title = `SVR Poker • ${LABEL}`;
   document.querySelectorAll(".pill").forEach(el=>{
     if ((el.textContent || "").includes("BUILD:")) el.textContent = `BUILD: ${LABEL}`;
@@ -22,7 +23,7 @@ function hideSupersededVisuals(){
   const patterns = [/PHASE176_LOBBY_ARENA/i,/PHASE176_JUMBOTRON/i,/CENTER_SPECTATOR_RING/i,/CENTER_FEATURED_TABLE_STAGE/i,/PHASE176_RING_LABEL/i,/PHASE179_CENTERPIECE/i,/PHASE183_ROMAN_MEZZANINE/i,/PHASE184_LOBBY_EXPERIENCE/i,/building/i,/skyline/i,/tower/i,/city/i,/adbuilding/i,/bannerbuilding/i,/phase123/i,/phase164/i,/phase173/i,/phase175/i];
   scene.traverse((obj)=>{
     const name = String(obj.name || "");
-    if (patterns.some(rx=>rx.test(name)) && !/PHASE185|PHASE187|PHASE181|PHASE180|PHASE178|PHASE177|PGA|REIKI|WELLNESS|SPONSOR|STORE|SCORPION|LEGEND|Watch|Teleport|Hand|Controller|Moon|Mars/i.test(name)) obj.visible = false;
+    if (patterns.some(rx=>rx.test(name)) && !/PHASE188|PHASE185|PHASE187|PHASE181|PHASE180|PHASE178|PHASE177|PGA|REIKI|WELLNESS|SPONSOR|STORE|SCORPION|LEGEND|Watch|Teleport|Hand|Controller|Moon|Mars/i.test(name)) obj.visible = false;
   });
 }
 
@@ -36,5 +37,6 @@ installPhase178Bounds();
 autoInstallPhase180TableSelector();
 autoInstallPhase185OfficialLobbyLook();
 installPhase186DeploySyncCleanup();
+autoInstallPhase188SecondFloorVisibility();
 setTimeout(hideSupersededVisuals, 1000);
 setTimeout(hideSupersededVisuals, 3000);
