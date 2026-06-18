1bf05a6a8af216d4f66f65a38ac963b735dda026
makaveli60629 <ronaldchadee@gmail.com>
2026-06-18 13:08:58 -0500

    Cache bust movement bridge for Phase 98 hand teleport

--- game/modules/movement_phase228.js
+++ game/modules/movement_phase228.js
@@ -1,1 +1,1 @@
-export { createTeleportRig } from "./movement_phase286_input_lock.js?v=phase286-input-priority";
+export { createTeleportRig } from "./movement_phase286_input_lock.js?v=phase98-hand-aim-release-bridge";
